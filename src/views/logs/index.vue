4f34a389a9b915bb12ba34d5eeb7060e9155d554
Ali Rıza BAKIR <67209912+Alirizabakir@users.noreply.github.com>
2023-08-04 23:57:16 +0300

    Fix aaa

---
+++ src/views/logs/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <!--begin::Logs-->
+  <div class="row g-5 g-xl-8 h-100">
+    <LogsTable page="Devices" url="devices"></LogsTable>
+  </div>
+  <!--end::Logs-->
+</template>
+
+<script>
+import {defineComponent} from "vue";
+import LogsTable from "@/components-ekds/logs/LogsTable.vue";
+
+export default defineComponent({
+  name: "logs",
+  components: {LogsTable},
+});
+</script>
